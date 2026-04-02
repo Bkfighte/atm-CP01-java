e7e619808cfbab31807084218598e2f3e3ed4cfa
Bruno Koeke <brunokoeke@gmail.com>
2026-04-02 12:57:12 -0300

    cadastro de senha forte

--- FiapBankAtm.java
+++ FiapBankAtm.java
@@ -10,6 +10,22 @@ public class FiapBankAtm {
         String primeiroNome = nomeCompleto.split(" ")[0];
         System.out.println("Ola, " + primeiroNome + "! Bem-vindo(a) ao FIAP Bank!");
 
+        // cadastro de senha
+        String senhaForte = "";
+        boolean senhaValida = false;
+
+        while (!senhaValida) {
+            System.out.print("Crie uma senha forte: ");
+            senhaForte = scanner.nextLine();
+
+            if (senhaForte.matches("^(?=.*[0-9])(?=.*[A-Z])(?=.*[!@#$%^&*()\\-_+=?><]).{8,}$")) {
+                senhaValida = true;
+                System.out.println("Senha cadastrada com sucesso!");
+            } else {
+                System.out.println("Senha fraca! A senha deve ter no minimo 8 caracteres, uma letra maiuscula, um numero e um caractere especial.");
+            }
+        }
+
         scanner.close();
     }
 }
